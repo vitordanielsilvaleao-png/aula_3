89770f5eb2059d9d04cfa915853903eb6f93228f
vitordanielsilvaleao-png <vitordanielsilvaleao@gmail.com>
2026-08-26 21:08:16 -0300

    Adiciona o método remover_projeto na classe ProjetoService e adiciona cometários em projeto.py e projeto_service.py

--- src/projeto/projeto.py
+++ src/projeto/projeto.py
@@ -1,9 +1,13 @@
+#Import da biblioteca datetime
 from datetime import datetime
 
+#Declaração da classe Projeto
 class Projeto:
 
+    #Variável para atualizar automaticamente o id
     prox_id = 1
 
+    #Declaração do construtor
     def __init__(self, nome, descricao, usuario):
         self.id = Projeto.prox_id
         self.nome = nome
--- src/projeto/projeto_service.py
+++ src/projeto/projeto_service.py
@@ -1,11 +1,15 @@
+#Import da classe Projeto do arquivo projeto.py
 from src.projeto.projeto import Projeto
 
+#Declaração da classe ProjetoService
 class ProjetoService:
 
+    #Declaração do Construtor
     def __init__(self, usuario_service):
         self.lista_projetos =[]
         self.usuario_service = usuario_service
 
+    #Método para adicionar projeto
     def criar_projeto(self, nome, descricao, usuario_id):
         usuario = self.usuario_service.buscar_usuario(usuario_id)
         registro_projeto = Projeto(nome, descricao, usuario)
@@ -13,6 +17,7 @@ class ProjetoService:
 
         return registro_projeto
 
+    #Método para listar projeto
     def listar_projetos(self):
 
         print("*******************************")
@@ -27,3 +32,15 @@ class ProjetoService:
             print("Nome do Usuário: ", registro_projeto.usuario.nome)
             print("E-mail do Usuário: ", registro_projeto.usuario.email)
             print("*******************************\n")
+
+    #Método para remover projeto
+    def remover_projeto(self, id):
+
+        for registro_projeto in self.lista_projetos:
+
+            if id == registro_projeto.id:
+                self.lista_projetos.remove(registro_projeto)
+                print("Removendo projeto ....... \nProjeto removido com sucesso!\n")
+                return
+
+        print("O projeto especificado não está cadastrado!\n")
