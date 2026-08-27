ae5cb03a02e9a8f721fa27726815d3c175e8d9af
vitordanielsilvaleao-png <vitordanielsilvaleao@gmail.com>
2026-08-26 22:18:28 -0300

    Adiciona os métodos marcar_concluida e esta_vencida na classe Tarefa

--- src/tarefa/tarefa.py
+++ src/tarefa/tarefa.py
@@ -1,4 +1,6 @@
 #Declaração da classe Tarefa
+from datetime import datetime
+
 
 class Tarefa:
 
@@ -14,3 +16,26 @@ class Tarefa:
         self.status = "EM ANDAMENTO"
         self.data_limite = data_limite
         self.projeto = projeto
+
+    #Método para marcar a tarefa como concluída
+    def marcar_concluida(self):
+        self.status = "CONCLUIDA"
+
+    #Método para validar se tarefa está vencida
+    def esta_vencida(self):
+
+        if self.status == "EM ANDAMENTO" or self.status == "PENDENTE":
+            data_limite = datetime.strptime(self.data_limite, "%d/%m/%Y")
+            data_atual = datetime.now()
+
+            if data_limite < data_atual:
+                self.status = "ATRASADO"
+                print("A tarefa está atrasada e seu status foi alterado!")
+                return True
+            else:
+                print("A tarefa está dentro do prazo!")
+                return False
+
+        else:
+            print("A tarefa já está concluída!")
+            return False
